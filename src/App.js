9cbae44c3a7c16b30e55168c146c0e5c130a4d79
Kelley Fraser <kelleyirwinfraser@gmail.com>
2022-05-24 10:43:58 +0100

    completed react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+import {marked} from 'marked';
 
 function App() {
+  const [markdown, setMarkdown] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="app-header">
+        <h1>Markdown Previewer</h1>
       </header>
+      <body className="app-body">
+        <div className="container editor-container">
+          <div className="header editor-header">Editor</div>
+          <textarea placeholder="Input markdown here..." autofocus id="editor" onChange={event => setMarkdown(event.target.value)}></textarea>
+        </div>
+        <div className="container preview-container">
+          <div className="header preview-header">Preview</div>
+          <div id="preview" dangerouslySetInnerHTML={{__html:marked(markdown)}}></div>
+        </div>
+        </body>  
     </div>
   );
 }
